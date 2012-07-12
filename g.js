ff75eab4f8c148da0f9e4e7fa3b299ad09e27420
Baptiste Fontaine <batifon@yahoo.fr>
2012-07-12 22:35:02 +0200

    little refactoring

--- g.js
+++ g.js
@@ -1,16 +1,15 @@
 (function(){
 
-var from = function(n) {
+var from = function(f) {
         return {
-            to: function(m) {
-                var f=n,t=m,s=1,incl=true;
+            to: function(t) {
+                var s=1,incl=true;
                 return {
                     by: function(_s) {s = _s;return this;},
                     excluded: function() {incl=false;return this;},
 
                     do: function(fn) {
-                        var i=f;
-                        for (;(s>0) ? i < t : i > t ;i+=s) {
+                        for (i=f;(s>0) ? i < t : i > t ;i+=s) {
                             fn.call(this, i);
                         }
                         if (incl && i == t) { fn.call(this, i); }
